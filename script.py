083546fad3bd5ce2af40d990b454c09096dd8317
Maijin <maijin21@gmail.com>
2017-04-28 22:51:34 +0200

    Update script.py

--- script.py
+++ script.py
@@ -11,6 +11,6 @@ for file in glob.glob("obj/*.obj"):
     r2.cmd('aaa')
     r2.cmd('zs '+ file[:-4].strip(sys.argv[1]+"/"))
     r2.cmd('zaF')
-    r2.cmd('zos masm32/masm32.zig.sdb')
-    r2.cmd('z >> masm32/masm32.zig.txt')
+    r2.cmd('zos masm32/masm32.zig')
+    r2.cmd('!gzip masm32/masm32.zig')
     r2.quit()
