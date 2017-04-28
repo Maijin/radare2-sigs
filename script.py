657de579e15c48f75443dc964f4bb24b20955c9e
Maijin <maijin21@gmail.com>
2017-04-28 23:00:35 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import r2pipe
-import sys, glob, os
+import sys, glob, os, shutil
+import gzip
 
 os.mkdir("masm32")
 for file in glob.glob("obj/*.obj"):
@@ -12,5 +13,8 @@ for file in glob.glob("obj/*.obj"):
     r2.cmd('zs '+ file[:-4].strip(sys.argv[1]+"/"))
     r2.cmd('zaF')
     r2.cmd('zos masm32/masm32.zig')
-    r2.cmd('!gzip masm32/masm32.zig')
     r2.quit()
+
+with open('masm32/masm32.zig', 'rb') as f_in:
+    with gzip.open('masm32/masm32.zig.gz', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
